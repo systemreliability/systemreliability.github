70608806d177bed55a07b95f82d8203a8ce9e001
systemreliability <51009183+systemreliability@users.noreply.github.com>
2025-04-10 21:46:49 +0000

    Deploying to gh-pages from @ systemreliability/systemreliability.github@1ef9010238f26eae05ff16b159382fe38bd43746 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/al-folio/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/al-folio/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-joined-the-structural-system-reliability-group-ssrg-at-snu",
+          title: 'I joined the Structural System Reliability Group (SSRG) at SNU.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
